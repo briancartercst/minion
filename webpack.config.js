aab436f157a0a0dc3eb590a9a181cea288b0f42f
Luis Crespo <lcrespo@es.ibm.com>
2015-11-25 15:18:04 +0100

    Added glob to bundle all controllers

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,25 @@
+var glob = require("glob");
+
+function getEntries() {
+	// Cool pure functional way
+	return ['./ts/main'].concat(
+		glob.sync('./ts/ctrl/*.ts').map(function(ctrl) {
+			return ctrl.substr(0, ctrl.length - 3)
+		})
+	);
+	// Uncool imperative way
+	/* var result = ['./ts/main'];
+	var controllers = glob.sync('./ts/ctrl/*.ts');
+	for (var i = 0; i < controllers.length; i++) {
+		var ctrl = controllers[i];
+		result.push(ctrl.substr(0, ctrl.length - 3));
+	}
+	return result; */
+}
+
 module.exports = {
 	devtool: 'source-map',
-	entry: ['./ts/main', './ts/ctrl/search'],
+	entry: getEntries(),
 	output: {
 		path: __dirname + '/js',
 		filename: 'bundle.js'
